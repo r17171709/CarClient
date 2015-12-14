3c5eeb7aa048e779ac8e904e81ccc620295ba200
renyu <Yuren2@hotmail.com>
2015-12-14 14:58:34 +0800

    完成基本页面功能

---
+++ app/src/main/java/com/renyu/carclient/activity/login/JoinActivity.java
@@ -0,0 +1,14 @@
+package com.renyu.carclient.activity.login;
+
+import com.renyu.carclient.R;
+import com.renyu.carclient.base.BaseActivity;
+
+/**
+ * Created by renyu on 15/12/14.
+ */
+public class JoinActivity extends BaseActivity {
+    @Override
+    public int initContentView() {
+        return R.layout.activity_join;
+    }
+}
--- app/src/main/java/com/renyu/carclient/activity/login/LoginActivity.java
+++ app/src/main/java/com/renyu/carclient/activity/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.renyu.carclient.activity;
+package com.renyu.carclient.activity.login;
 
 import android.os.Bundle;
 import android.util.Log;
